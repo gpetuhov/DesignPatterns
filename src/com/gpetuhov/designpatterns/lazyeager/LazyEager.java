1c1f8147deb76fa1e6fb74220bd1b4adeca86c97
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-08-06 14:42:33 +0300

    Add LazyEager

---
+++ src/com/gpetuhov/designpatterns/lazyeager/LazyEager.java
@@ -0,0 +1,40 @@
+package com.gpetuhov.designpatterns.lazyeager;
+
+// Lazy initialization is done on demand,
+// when the object is needed.
+class Lazy {
+    private static Lazy instance = null;
+
+    public static Lazy getInstance() {
+        if (instance == null) {
+            instance = new Lazy();
+        }
+
+        return instance;
+    }
+
+    public String getType() {
+        return "Lazy";
+    }
+}
+
+// This is called eager initialization.
+// Instance is created, when the class is loaded into the JVM.
+class Eager {
+    private static final Eager instance = new Eager();
+
+    public static Eager getInstance() {
+        return instance;
+    }
+
+    public String getType() {
+        return "Eager";
+    }
+}
+
+public class LazyEager {
+    public static void main(String[] args) {
+        System.out.println(Lazy.getInstance().getType());
+        System.out.println(Eager.getInstance().getType());
+    }
+}
