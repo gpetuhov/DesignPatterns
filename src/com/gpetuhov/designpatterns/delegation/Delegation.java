fd02066f7c7eaf3e112400dce541c5fd80f22682
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-08-06 14:45:25 +0300

    Add Delegation

---
+++ src/com/gpetuhov/designpatterns/delegation/Delegation.java
@@ -0,0 +1,30 @@
+package com.gpetuhov.designpatterns.delegation;
+
+// This is the one, that really does the job of making sound
+class Sound {
+    public String makeSound() {
+        return "Meow";
+    }
+}
+
+// This one delegates the job of making sound to the instance of the Sound class
+class Animal {
+    // This field is called the delegate object
+    private Sound sound;
+
+    Animal(Sound sound) {
+        this.sound = sound;
+    }
+
+    public String makeSound() {
+        // Here is the delegation
+        return sound.makeSound();
+    }
+}
+
+public class Delegation {
+    public static void main(String[] args) {
+        Animal animal = new Animal(new Sound());
+        System.out.println(animal.makeSound());
+    }
+}
